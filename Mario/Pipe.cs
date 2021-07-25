14a722fe8fcf8d11b483cb8a5f8d6b0e0d87f08c
ndnestor <Nathan.nestor@outlook.com>
2021-07-24 20:08:19 -0400

    Implemented pipe reading and read callback

--- Mario/Pipe.cs
+++ Mario/Pipe.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.IO.Pipes;
@@ -11,7 +12,7 @@ namespace Mario {
 		// Pipe settings
 		private readonly Process _targetProcess;
 		private readonly bool _isBidirectional; // Otherwise it only acts as an input
-		private readonly int _flowInterval; // In milliseconds
+		private readonly Action<string[]> _flowInCallback;
 
 		// Pipe infrastructure
 		private AnonymousPipeServerStream _pipeIn;
@@ -22,45 +23,56 @@ namespace Mario {
 		private Task _readPipeTask;
 		
 		// Constructor
-		public Pipe(Process targetProcess, bool isBidirectional, int flowInterval) {
+		public Pipe(Process targetProcess, bool isBidirectional, int flowInterval, Action<string[]> flowInCallback) {
 			
 			// Set pipe settings
 			_targetProcess = targetProcess;
 			_isBidirectional = isBidirectional;
-			_flowInterval = flowInterval;
-			
-			// Initialize pipe infrastructure
-			_pipeIn = new AnonymousPipeServerStream(PipeDirection.In, HandleInheritability.Inheritable);
-			_pipeOut = new AnonymousPipeServerStream(PipeDirection.Out, HandleInheritability.Inheritable);
-			_pipeInReader = new StreamReader(_pipeIn);
-			_pipeOutWriter = new StreamWriter(_pipeOut);
+			_flowInCallback = flowInCallback;
 
-			// Set up timer that reads pipe every ReadPipeInterval milliseconds
-			_readPipeTimer.Interval = _flowInterval;
-			_readPipeTimer.Elapsed += Flow;
-			_readPipeTimer.AutoReset = true;
-			_readPipeTimer.Start();
+			// Initialize pipe infrastructure
+			_pipeOut = new AnonymousPipeServerStream(PipeDirection.Out, HandleInheritability.Inheritable);
+			_pipeOutWriter = new StreamWriter(_pipeOut);
+			if(_isBidirectional) {
+				_pipeIn = new AnonymousPipeServerStream(PipeDirection.In, HandleInheritability.Inheritable);
+				_pipeInReader = new StreamReader(_pipeIn);
+
+
+				// Set up timer that reads pipe every ReadPipeInterval milliseconds
+				_readPipeTimer.Interval = flowInterval;
+				_readPipeTimer.Elapsed += FlowIn;
+				_readPipeTimer.AutoReset = true;
+			}
 		}
 
 		// Starts the target process and the transfer of data
 		public void Start() {
 			_targetProcess.Start();
-			
-			// TODO: Call Flow() periodically
-			
+			_readPipeTimer.Start();
 		}
-
-		private void Flow() {
-			FlowOut();
-			if(_isBidirectional) {
-				FlowIn();
+		
+		// Read from the pipe
+		private void FlowIn(object sender, ElapsedEventArgs e) {
+			
+			// Check for sync message
+			string pipeMessage = _pipeInReader.ReadLine();
+			if(pipeMessage == null || !pipeMessage.StartsWith("SYNC")) {
+				
+				// No message was found
+				return;
 			}
-		}
-
-		private void FlowIn() {
 			
+			// Get all messages in the pipe
+			List<string> pipeMessageLines = new();
+			do {
+				pipeMessage = _pipeInReader.ReadLine();
+				pipeMessageLines.Add(pipeMessage);
+			} while(pipeMessage != null && !pipeMessage.StartsWith("END"));
+
+			_flowInCallback(pipeMessageLines.ToArray());
 		}
 
+		// Write to the pipe
 		private void FlowOut() {
 			
 		}
